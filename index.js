a00ff7e5fa991acccafaeec993cd6188332e0e12
Lord Tech / Lord Samuel <lordsamueltech@gmail.com>
2025-05-16 10:52:24 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express();
 const path = require('path')
 
 
+app.use(express.static(path.join(__dirname, 'index.html')))
+app.use(express.static(path.join(__dirname, 'favicon')))
+app.use(express.static(path.join(__dirname, 'css')))
+app.use(express.static(path.join(__dirname, 'js')))
+app.use(express.static(path.join(__dirname, 'images')))
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
